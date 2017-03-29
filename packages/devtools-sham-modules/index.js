a0f189da5a0cb602612867a4ec4df699539d0f9d
Fred Lin <gasolin@mozilla.com>
2017-03-29 11:17:16 +0800

    Bundle missed devtools-sham-modules for netmonitor

--- packages/devtools-sham-modules/index.js
+++ packages/devtools-sham-modules/index.js
@@ -1,14 +1,18 @@
 const AppConstants = require("./sham/appconstants");
+const { Chart } = require("./client/shared/widgets/Chart");
 const { DebuggerClient } = require("./shared/client/main");
 const { DebuggerTransport } = require("./transport/transport");
 const DevToolsUtils = require("./shared/DevToolsUtils");
 const EventEmitter = require("./shared/event-emitter");
 const frame = require("./client/shared/components/frame");
+const { HTMLTooltip } = require("./client/shared/widgets/tooltip/HTMLTooltip");
+const { KeyCodes } = require("./client/shared/keycodes");
 const { KeyShortcuts } = require("./client/shared/key-shortcuts");
 const Menu = require("./client/framework/menu");
 const MenuItem = require("./client/framework/menu-item");
 const PrefsHelper = require("./client/shared/prefs").PrefsHelper;
 const SearchBox = require("./client/shared/components/search-box");
+const { setNamedTimeout } = require("./client/shared/widgets/view-helpers");
 const sourceUtils = require("./client/shared/source-utils");
 const Tabbar = require("./client/shared/components/tabs/tabbar");
 const TabPanel = require("./client/shared/components/tabs/tabs");
@@ -18,17 +22,21 @@ const WebsocketTransport = require("./shared/transport/websocket-transport");
 
 module.exports = {
   AppConstants,
+  Chart,
   frame,
   DebuggerClient,
   DebuggerTransport,
   DevToolsUtils,
   EventEmitter,
+  HTMLTooltip,
+  KeyCodes,
   KeyShortcuts,
   Menu,
   MenuItem,
   PrefsHelper,
   sourceUtils,
   SearchBox,
+  setNamedTimeout,
   Tabbar,
   TabPanel,
   TargetFactory,
